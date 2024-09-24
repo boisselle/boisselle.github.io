487f10ee5a3ceb516fc1a9edc39871adefe29f6e
Boisselle <sean@boisselle.org>
2024-09-24 10:14:52 -0700

    testing deployment...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://boisselle.github.io',
+    base: '/boisselle.github.io/',
+    integrations: [
+        // Enable clean URLs for better routing in GitHub Pages
+        { cleanUrls: true },
+      ],
 });
