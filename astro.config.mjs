9932e1333688bebe8a672ffaa2634c6345aa8e46
Boisselle <sean@boisselle.org>
2024-09-24 16:55:22 -0700

    updating config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/boisselle.github.io/',
-    cleanUrls: true, // Move cleanUrls here
-    // Remove the integrations array if it's empty or add valid integrations
+    // base: '/boisselle.github.io/',
+    cleanUrls: true,
   });
   
